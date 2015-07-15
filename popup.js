c759a9d99d8d6e14626f02fe7b78638c1f89c0a0
Willian Reis <reiswn@gmail.com>
2015-07-14 23:32:20 -0300

    Inner Table e Assync request

--- popup.js
+++ popup.js
@@ -1,14 +1,32 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://api.cartola.globo.com/time_adversario/se-shitara.json", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    var resp = JSON.parse(xhr.responseText);
-    document.getElementById("demo").innerHTML =
-      resp.atletas[0].apelido + "<br>" +
-      resp.atletas[1].apelido + "<br>" +
-      resp.time.nome;
-  }
-}
-xhr.send();
+function iterate_jason(rodada){
+  
+  var xhr = new XMLHttpRequest();
+  
+  xhr.open("GET", "http://cartolafc.globo.com/time/moonra-fc/atletas.json?rodada_id="+rodada, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var resp = JSON.parse(xhr.responseText);
+      
+      var pElement = document.createElement("tr");  
 
+      var td = pElement.appendChild(document.createElement("td"));
+      td.innerHTML = resp.time.rodada;
+      
+      var td = pElement.appendChild(document.createElement("td"));
+      td.innerHTML = resp.time.pontuacao;
+      
+      var td = pElement.appendChild(document.createElement("td"));
+      td.innerHTML = resp.time.patrimonio;
+
+      document.getElementById("demo").appendChild(pElement);
+
+    }
+  }
+  xhr.send();
+
+}
+
+for(var i = 1; i<14; i++){
+  iterate_jason(i);
+}
