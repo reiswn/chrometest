6116914e033b2c74815f8131a459d56f71d96977
Willian <reiswn@gmail.com>
2015-07-21 17:49:04 -0300

    add new things

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 var labelsRodadas = [];
 var arrPontuacao = [];
-var esquemas = ["3-4-3","3-5-2","4-3-3","4-4-2","4-5-1","5-3-2","5-4-1"]
+var esquemas = ["3-4-3","3-5-2","4-3-3","4-4-2","4-5-1","5-3-2","5-4-1"];
 var datasetValue = [];
+var times = ["acerri-fc", "se-shitara", "moonra-fc"];
 
 
 function iterate_jason(rodada, time){
@@ -39,80 +40,37 @@ function iterate_jason(rodada, time){
 
 document.getElementById("pesquisar").addEventListener('click', function() {
 
+  for(var j = 0; j < times.length; j++){
 
-    for(var i = 1; i<14; i++){
-      //iterate_jason(i, document.getElementById("time").value);
-      iterate_jason(i, "se-shitara");
+    arrPontuacao = [];
+    
+    for(var i = 1; i<document.getElementById("rodadas").value; i++){
+      iterate_jason(i, times[j]);
       labelsRodadas[i-1] = i;
     }
 
-    datasetValue[0] = 
-      {
-				label: "Dataset se-shitara",
-				fillColor : "rgba(151,187,205,0.2)",
-				strokeColor : "rgba(151,187,205,1)",
-				pointColor : "rgba(151,187,205,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(151,187,205,1)",
-				data : arrPontuacao
-			}
-      
-    arrPontuacao = [];
-    for(var i = 1; i<14; i++){
-      iterate_jason(i, "moonra-fc");
-    }
-    
-    datasetValue[1] = 
-      {
-				label: "Dataset moonra-fc",
-				fillColor : "rgba(130,220,130,0.2)",
-				strokeColor : "rgba(130,220,130,1)",
-				pointColor : "rgba(130,220,130,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(130,220,130,1)",
-				data : arrPontuacao
-			}
+    datasetValue[j] = 
+    {
+		label: "Dataset "+j,
+		fillColor : "rgba(151,187,205,0.2)",
+		strokeColor : "rgba(151,187,205,1)",
+		pointColor : "rgba(151,187,205,1)",
+		pointStrokeColor : "#fff",
+		pointHighlightFill : "#fff",
+		pointHighlightStroke : "rgba(151,187,205,1)",
+		data : arrPontuacao
+	}
 
-      arrPontuacao = [];
-    for(var i = 1; i<14; i++){
-      iterate_jason(i, "acerri-fc");
-    }
-    
-    datasetValue[2] = 
-      {
-				label: "Dataset acerri",
-				fillColor : "rgba(220,220,220,0.2)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : arrPontuacao
-			}
-    
-    var lineChartData = {
-			labels : labelsRodadas,
-			datasets : datasetValue
-    }
-    
-    var ctx = document.getElementById("grafico").getContext("2d");
-		window.myLine = new Chart(ctx).Line(lineChartData, {
-			responsive: true
-		});
-    
-    //alert("Datasets: "+lineChartData.datasets[1].data);
+  }
 
+  var lineChartData = {
+    labels : labelsRodadas,
+	datasets : datasetValue
+  }
+    
+  var ctx = document.getElementById("grafico").getContext("2d");
+  window.myLine = new Chart(ctx).Line(lineChartData, {
+    responsive: true
+  });
+ 
 });
-
-				//{
-					//label: "My First dataset",
-					//fillColor : "rgba(220,220,220,0.2)",
-					//strokeColor : "rgba(220,220,220,1)",
-					//pointColor : "rgba(220,220,220,1)",
-					//pointStrokeColor : "#fff",
-					//pointHighlightFill : "#fff",
-					//pointHighlightStroke : "rgba(220,220,220,1)",
-//					data : arrPontuacao
-				//},
